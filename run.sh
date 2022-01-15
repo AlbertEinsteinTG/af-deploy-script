6a202856028d3b7243d3ea8b5d959085ad375d43
Albert Einstein <73480087+AlbertEinsteinTG@users.noreply.github.com>
2022-01-15 19:07:16 +0530

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 export TERM=xterm;
 
-git clone https://github.com/CrazyBotsz/Adv-Auto-Filter-Bot-V2 Auto_Filter
+git clone "${REPO_URL:-https://github.com/CrazyBotsz/Adv-Auto-Filter-Bot-V2}" Auto_Filter
 cd Auto_Filter
 echo "Starting...!";
 python3 -m bot
