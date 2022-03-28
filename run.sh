83895cbd4f376db25a7882eb3d25e80035a98afc
screeeeeee <102519114+screeeeeee@users.noreply.github.com>
2022-03-28 16:05:42 +0530

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 export TERM=xterm;
 
-git clone "${REPO_URL:-https://github.com/CrazyBotsz/Adv-Auto-Filter-Bot-V2}" Auto_Filter
+git clone "${REPO_URL:-https://github.com/screeeeeee/Adv-Auto-Filter-Bot-V2.git}" Auto_Filter
 cd Auto_Filter
 echo "Starting...!";
 python3 -m bot
